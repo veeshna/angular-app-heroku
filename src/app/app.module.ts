ab3a31fbc9957bf704bf4e127d0d01341d9d0984
behol <arnet.e@nintrivalabs.com>
2022-10-29 20:11:00 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AccordionModule } from 'primeng/accordion';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UserManagementModule } from './core/user-management/user-management.module';
 import { CommonModule } from '@angular/common';
 import { sharedModule } from './core/shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FeatureModule } from './core/feature/feature.module';
 
+import { LoginComponent } from './core/user-management/login/login.component';
+// import { PanelModule, InputTextModule, ButtonModule } from 'primeng';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+
+import { PanelModule } from 'primeng/panel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -22,9 +27,14 @@ import { FeatureModule } from './core/feature/feature.module';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    UserManagementModule,
     sharedModule,
     FeatureModule,
+    InputTextModule,
+    CardModule,
+    DialogModule,
+    ButtonModule,
+    PanelModule,
+    UserManagementModule,
   ],
   exports: [],
   providers: [],
--- src/app/core/user-management/login/login.component.ts
+++ src/app/core/user-management/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  // styles: ["body { background: rgb(2, 2, 46); }"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    login: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  submitted = false;
 
-  ngOnInit(): void {
+  loginProfile: FormGroup = new FormGroup({
+    user: new FormControl(''),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.submitted = true;
+    alert(JSON.stringify(this.loginForm.value));
   }
-
 }
