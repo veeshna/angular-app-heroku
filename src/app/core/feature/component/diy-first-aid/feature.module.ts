60cde4bddb6ff69f0b6f1f66705087e6da415a91
Chanchel <chanchelvs4@gmail.com>
2022-10-29 16:36:54 +0530

    updated

--- src/app/core/feature/component/diy-first-aid/feature.module.ts
+++ src/app/core/feature/component/diy-first-aid/feature.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
+import { CardModule } from 'primeng/card';
 import { DiyFirstAidComponent } from './diy-first-aid.component';
 
 @NgModule({
   declarations: [DiyFirstAidComponent],
-  imports: [AccordionModule],
+  imports: [AccordionModule, CardModule],
   providers: [],
   bootstrap: [],
 })
