c6f556256c309ec7262e8c17beed9cd720753aeb
Ramth95 <rammohan.th@nintrivalabs.com>
2022-10-29 19:00:59 +0530

    redeploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ambulance-tracker'));
+app.use(express.static('./dist/app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ambulance-tracker/'}),
+    res.sendFile('index.html', {root: 'dist/app/'}),
 );
 
 // Start the app by listening on the default Heroku port
