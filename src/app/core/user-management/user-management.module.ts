c43b4951d7b856fb249d464e297fff67d2a260e8
behol <arnet.e@nintrivalabs.com>
2022-10-29 13:06:18 +0530

    app-structure

---
+++ src/app/core/user-management/user-management.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { LoginComponent } from "./login/login.component";
+
+@NgModule({
+    declarations: [
+      LoginComponent
+    ],
+    imports: [
+     
+     
+    
+    ],
+    providers: [],
+    bootstrap: []
+  })
+  export class UserManagementModule { }
+  
