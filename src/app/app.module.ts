278846f1b00a7de0b3a7623351ebab6a084d4881
Ramth95 <rammohanth@gmail.com>
2022-10-29 16:22:59 +0530

    Add first aid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AccordionModule } from 'primeng/accordion';
+import {CardModule} from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UserManagementModule } from './core/user-management/user-management.module';
 import { CommonModule } from '@angular/common';
+import { DiyFirstAidComponent } from './core/feature/component/diy-first-aid/diy-first-aid.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,DiyFirstAidComponent
   ],
   imports: [
     CommonModule,
@@ -20,8 +23,10 @@ import { CommonModule } from '@angular/common';
     ReactiveFormsModule,
     FormsModule,
     UserManagementModule,
+    AccordionModule,
+    CardModule,
+    ButtonModule
   ],
-  exports:[],
   providers: [],
   bootstrap: [AppComponent]
 })
