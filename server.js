747f1081b1a2f130cbb29f5618c1952feda15a0b
Ramth95 <rammohan.th@nintrivalabs.com>
2022-10-30 11:20:22 +0530

    fix deployment bug

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app'));
+app.use(express.static('./dist/trustme'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app/'}),
+    res.sendFile('index.html', {root: 'dist/trustme/'}),
 );
 
 // Start the app by listening on the default Heroku port
